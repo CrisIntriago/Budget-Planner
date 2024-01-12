dae7f84d1fc503a1e9c563efc233dddf4b236eca
CrisIntriago <caintria@espol.edu.ec>
2024-01-12 12:24:37 -0500

    Funcionando logica de cálculo

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 
-const ControlPresupuesto = ({presupuesto}) => {
+const ControlPresupuesto = ({presupuesto, gastos}) => {
+
+  const [disponible, setDisponible ]= useState(presupuesto);
+  const [gastado, setGastado]= useState(0);
+
+  useEffect(() => {
+    
+    let gastoTotal= 0;
+    for( let i = 0 ; i < gastos.length; i++)
+      gastoTotal += gastos[i].cantidad
+
+    setGastado(gastoTotal);
+    setDisponible(presupuesto - gastoTotal);
+  }, [gastos])
+
+  const formatearCantidad= (cantidad) =>{
+    return cantidad.toLocaleString("en-US",{
+      style: "currency",
+      currency: "USD"
+    })
+  }
+
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <p>Grafica aquí</p>
 
       <div className='contenido-presupuesto'>
         <p>
-          <span>Presupuesto: </span> ${presupuesto}
+          <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+        </p>
+        <p>
+          <span>Disponible: </span> {formatearCantidad(disponible)}
+        </p>
+        <p>
+          <span>Gastadoo: </span> {formatearCantidad(gastado)}
         </p>
       </div>
     </div> 
